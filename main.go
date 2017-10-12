d5431545ae0fc28ac85c3c26944b23abe1102784
wiffel <wiffel@tilient.net>
2017-10-12 19:14:22 +0200

    demo

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"log"
 	"time"
+	"sync"
 
 	"github.com/tarm/serial"
 )
@@ -16,13 +17,14 @@ import (
 type mbot struct {
 	port *serial.Port
 	cmd  []byte
+	mux  sync.Mutex
 }
 
 func makeMbot(portname string) *mbot {
 	bot := mbot{}
 	c := &serial.Config{
 		Name: portname,
-		Baud: 57600,
+		Baud: 9600, //19200,
   	ReadTimeout: 500 * time.Millisecond}
 	p, err := serial.OpenPort(c)
 	if err != nil {
@@ -34,18 +36,33 @@ func makeMbot(portname string) *mbot {
 }
 
 func (bot *mbot)close() {
+	bot.mux.Lock()
+	defer bot.mux.Unlock()
+	bot.port.Flush()
 	bot.cmd = bot.cmd[0:0]
 	bot.port.Close()
 }
 
 func (bot *mbot) addCmd(cmd ...byte) {
+	bot.mux.Lock()
   bot.cmd = append(bot.cmd, cmd...)
+  //bot.cmd = append(bot.cmd, 0x00, 0x00)
+	bot.mux.Unlock()
 }
 
 func (bot *mbot) sendCmd() {
-	_, err := bot.port.Write(bot.cmd)
+	if len(bot.cmd) < 1 {
+		return
+	}
+	//fmt.Println("sendCmd:", bot.cmd)
+	bot.mux.Lock()
+	defer bot.mux.Unlock()
+	n, err := bot.port.Write(bot.cmd)
 	if err != nil {
 		log.Fatal(err)
+	}
+	if n < len(bot.cmd) {
+		log.Fatal(n, "is smaller then ", len(bot.cmd))
 	}
 	err = bot.port.Flush()
 	if err != nil {
@@ -85,38 +102,72 @@ const (
 )
 
 func (bot *mbot)motorCmd(motor byte, speed int16) {
+	if motor == rightMotor {
+		speed = -speed
+	}
 	bot.addCmd(0xff, 0x55, 0x06, 0x60, 0x02, 0x0a, motor,
              byte(speed & 0xff), byte((speed >> 8) & 0xff))
 }
 
 // ----------------------------------------------------------
 
+func rotateTest(bot *mbot, wg *sync.WaitGroup) {
+	for i := 0; i < 2; i++ {
+    bot.motorCmd(rightMotor, 200)
+    bot.motorCmd(leftMotor, 200)
+    bot.sendCmd()
+    time.Sleep(500 * time.Millisecond)
+    bot.motorCmd(rightMotor, -200)
+    bot.motorCmd(leftMotor, -200)
+    bot.sendCmd()
+    time.Sleep(500 * time.Millisecond)
+  }
+  bot.motorCmd(rightMotor, 0)
+  bot.motorCmd(leftMotor, 0)
+  bot.sendCmd()
+  wg.Done()
+}
+
+func sireneTest(bot *mbot, wg *sync.WaitGroup) {
+  time.Sleep(100 * time.Millisecond)
+	for t := 0; t < 8; t++ {
+		bot.buzzerCmd(6000, 80)
+		time.Sleep(200 * time.Millisecond)
+		bot.sendCmd()
+		bot.buzzerCmd(3000, 80)
+		time.Sleep(200 * time.Millisecond)
+		bot.sendCmd()
+	}
+  wg.Done()
+}
+
+func blinkTest(bot *mbot, wg *sync.WaitGroup) {
+  time.Sleep(200 * time.Millisecond)
+  for t := 0; t < 8; t++ {
+  	bot.ledCmd(ledLeft, 0xa0, 0x00, 0x00)
+  	bot.ledCmd(ledRight, 0x00, 0x00, 0xa0)
+  	time.Sleep(200 * time.Millisecond)
+  	bot.sendCmd()
+  	bot.ledCmd(ledLeft, 0x00, 0x00, 0xa0)
+  	bot.ledCmd(ledRight, 0xa0, 0x00, 0x00)
+  	time.Sleep(200 * time.Millisecond)
+  	bot.sendCmd()
+  }
+  bot.ledCmd(ledBoth, 0x00, 0x00, 0x00)
+  bot.sendCmd()
+  wg.Done()
+}
+
 func main() {
 	fmt.Println("--- mBot ---")
 	bot := makeMbot("COM4")
 	defer bot.close()
 
-	//bot.motorCmd(leftMotor, -50)
-	//bot.motorCmd(rightMotor, 50)
-  //bot.sendCmd()
-  //time.Sleep(500 * time.Millisecond)
-	//bot.motorCmd(leftMotor, 0)
-	//bot.motorCmd(rightMotor, 0)
-  //bot.sendCmd()
-	for t := 0; t < 16; t++ {
-		bot.buzzerCmd(6000, 80)
-		bot.ledCmd(ledLeft, 0xa0, 0x00, 0x00)
-		bot.ledCmd(ledRight, 0x00, 0x00, 0xa0)
-		time.Sleep(200 * time.Millisecond)
-		bot.sendCmd()
-		bot.buzzerCmd(3000, 80)
-		bot.ledCmd(ledLeft, 0x00, 0x00, 0xa0)
-		bot.ledCmd(ledRight, 0xa0, 0x00, 0x00)
-		time.Sleep(200 * time.Millisecond)
-		bot.sendCmd()
-	}
-	bot.ledCmd(ledBoth, 0x00, 0x00, 0x00)
-	bot.sendCmd()
-
+	wg := sync.WaitGroup{}
+	wg.Add(3)
+  go rotateTest(bot, &wg)
+  go sireneTest(bot, &wg)
+  go blinkTest(bot, &wg)
+	wg.Wait()
 	fmt.Println("------------")
 }
