adc26b06e25512778d65d469053414fa33a510e1
wiffel <wiffel@tilient.net>
2017-10-15 11:06:37 +0200

    fixes

--- examples/test01.go
+++ examples/test01.go
@@ -1,14 +1,11 @@
 package main
 
-/* cfr.
-   http://learn.makeblock.com/en/...
-     ...mbot-serial-port-protocol/
-
-to compile for windows
-
-export GOOS=windows
-export GOARCH=amd64
-export CC=x86_64-w64-mingw32-gcc
+/*
+to cross-compile for windows under linux:
+  export GOOS=windows
+  export GOARCH=amd64
+  export CC=x86_64-w64-mingw32-gcc
+  go build test01.go
 */
 
 import (
@@ -19,9 +16,31 @@ import (
 	"github.com/tilient/mbot"
 )
 
-// ------------------------------------------------
+// ------------------------------------------------------------
+
+func main() {
+	fmt.Println("--- mbot ---")
+
+	bot := mbot.MakeMbot("COM5")
+	defer bot.Close()
+
+	wg := &sync.WaitGroup{}
+	wg.Add(6)
+	go rotateTest(bot, wg)
+	go sireneTest(bot, wg)
+	go blinkTest(bot, wg)
+	go lineSensorTest(bot, wg)
+	go ultrasonicSensorTest(bot, wg)
+	go lightSensorTest(bot, wg)
+	wg.Wait()
+
+	fmt.Println("------------")
+}
+
+// ------------------------------------------------------------
 
 func rotateTest(bot *mbot.Mbot, wg *sync.WaitGroup) {
+	defer wg.Done()
 	bot.MotorCmd(mbot.RightMotor, 100)
 	bot.MotorCmd(mbot.LeftMotor, -100)
 	time.Sleep(1500 * time.Millisecond)
@@ -33,22 +52,21 @@ func rotateTest(bot *mbot.Mbot, wg *sync.WaitGroup) {
 	time.Sleep(1500 * time.Millisecond)
 	bot.MotorCmd(mbot.RightMotor, 0)
 	bot.MotorCmd(mbot.LeftMotor, 0)
-	wg.Done()
 }
 
 func sireneTest(bot *mbot.Mbot, wg *sync.WaitGroup) {
+	defer wg.Done()
 	time.Sleep(2000 * time.Millisecond)
-	for t := 0; t < 4; t++ {
+	for t := 0; t < 7; t++ {
 		bot.BuzzerCmd(6000, 80)
 		time.Sleep(200 * time.Millisecond)
 		bot.BuzzerCmd(3000, 80)
 		time.Sleep(200 * time.Millisecond)
 	}
-	wg.Done()
 }
 
 func blinkTest(bot *mbot.Mbot, wg *sync.WaitGroup) {
-	time.Sleep(200 * time.Millisecond)
+	defer wg.Done()
 	for t := 0; t < 12; t++ {
 		bot.LedCmd(mbot.LedLeft, 0xa0, 0x00, 0x00)
 		bot.LedCmd(mbot.LedRight, 0x00, 0x00, 0xa0)
@@ -58,12 +76,12 @@ func blinkTest(bot *mbot.Mbot, wg *sync.WaitGroup) {
 		time.Sleep(200 * time.Millisecond)
 	}
 	bot.LedCmd(mbot.LedBoth, 0x00, 0x00, 0x00)
-	wg.Done()
 }
 
 func lineSensorTest(bot *mbot.Mbot, wg *sync.WaitGroup) {
+	defer wg.Done()
 	time.Sleep(1000 * time.Millisecond)
-	for t := 0; t < 25; t++ {
+	for t := 0; t < 20; t++ {
 		right, left := bot.LineSensorCmd()
 		if left {
 			bot.LedCmd(mbot.LedLeft, 0x00, 0xa0, 0x00)
@@ -78,42 +96,24 @@ func lineSensorTest(bot *mbot.Mbot, wg *sync.WaitGroup) {
 		time.Sleep(200 * time.Millisecond)
 	}
 	bot.LedCmd(mbot.LedBoth, 0x00, 0x00, 0x00)
-	wg.Done()
 }
 
 func ultrasonicSensorTest(bot *mbot.Mbot, wg *sync.WaitGroup) {
-	time.Sleep(1000 * time.Millisecond)
-	for t := 0; t < 50; t++ {
+	defer wg.Done()
+	time.Sleep(2000 * time.Millisecond)
+	for t := 0; t < 30; t++ {
 		val := bot.UltrasonicSensorCmd()
 		fmt.Println("ultrasonic:", val)
 		time.Sleep(100 * time.Millisecond)
 	}
-	wg.Done()
 }
 
 func lightSensorTest(bot *mbot.Mbot, wg *sync.WaitGroup) {
-	time.Sleep(1000 * time.Millisecond)
-	for t := 0; t < 50; t++ {
+	defer wg.Done()
+	time.Sleep(3000 * time.Millisecond)
+	for t := 0; t < 20; t++ {
 		val := bot.LigtSensorCmd()
 		fmt.Println("light:", val)
 		time.Sleep(100 * time.Millisecond)
 	}
-	wg.Done()
-}
-
-func main() {
-	fmt.Println("--- mbot ---")
-	bot := mbot.MakeMbot("COM5")
-	defer bot.Close()
-
-	wg := sync.WaitGroup{}
-	wg.Add(4)
-	//go rotateTest(bot, &wg)
-	//go sireneTest(bot, &wg)
-	go blinkTest(bot, &wg)
-	go lineSensorTest(bot, &wg)
-	go ultrasonicSensorTest(bot, &wg)
-	go lightSensorTest(bot, &wg)
-	wg.Wait()
-	fmt.Println("------------")
 }
