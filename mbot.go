1f5f03a168f0dc77f0c21d317ebc55ab61d4f06a
wiffel <wiffel@tilient.net>
2017-10-15 11:07:38 +0200

    fixes

--- mbot.go
+++ mbot.go
@@ -1,15 +1,5 @@
 package mbot
 
-// cfr.
-//   http://learn.makeblock.com/en/...
-//     ...mbot-serial-port-protocol/
-//
-/* to compile for windows
-export GOOS=windows
-export GOARCH=amd64
-export CC=x86_64-w64-mingw32-gcc
-*/
-
 import (
 	"encoding/binary"
 	"log"
